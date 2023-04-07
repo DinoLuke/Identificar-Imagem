03d40b93f3b12e2d60ec4ce114c436a21f53b5c4
DinoLuke <112709256+DinoLuke@users.noreply.github.com>
2023-04-07 18:45:32 -0300

    Add files via upload
    AÉ

---
+++ main.js
@@ -0,0 +1,50 @@
+var synth = window.speechSynthesis;
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+  camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+      
+function takeSnapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+  
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/UBv6GZftN/model.json',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    
+    speak_data= "Este objeto é " + results[0].label;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+    console.log(results);
+    document.getElementById("resultObjectName").innerHTML = results[0].label;
+    document.getElementById("resultObjectAccuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
